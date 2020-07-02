e5bf45fd6d49ad7aae386cf41ec190b404d36d21
Karthik <67741161+Karthik2102@users.noreply.github.com>
2020-07-02 17:37:01 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,9 @@
+1.First I will go to directory in which the file is located.For example if file is in D drive then i will use command
+D:
+2.Then i will check whether file exists in directory with command : D:>dir
+3.It will display the files in directory and we can check whether our file exists in our directory or not by using command - filename.txt
+4. Then we can type command as : type Filename.txt (which will dispplay list of files in our text file) and it gives the contents of the filename which we have given
+commands used:
+D:
+D:\>dir
+D:\>type Filename.txt
